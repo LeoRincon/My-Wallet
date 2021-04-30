9b8ee3c1f223f22e7d2571f9ffe51118052e3bef
Leonardo Rincón <jleobotache@gmail.com>
2021-04-30 15:04:27 -0500

    styles and modal creation settings with styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
+const btnDarkMode = document.getElementById("mode-dark");
+const bodyDarkMode = document.querySelector("body");
+const containerModal = document.querySelector(".container-modal");
+const modal = document.querySelector(".modal");
+const btnSignUp = document.getElementById("signup");
+const btnCloseModal = document.getElementById("modal-close");
 
+const activeModeDark = () => {
+  btnDarkMode.classList.toggle("active__dark-odel");
+  bodyDarkMode.classList.toggle("body__dark-model");
+};
+
+const activeModal = () => {
+  containerModal.classList.toggle("active");
+  modal.classList.toggle("modal-active");
+};
+
+btnDarkMode.addEventListener("click", activeModeDark);
+btnSignUp.addEventListener("click", activeModal);
+btnCloseModal.addEventListener("click", activeModal);
